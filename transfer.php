5329b72e0c08ff9fc8343e1e4733450f3e81b8f0
cesardanielhg <cesardanielhg@gmail.com>
2018-03-29 17:14:05 -0600

    trasnfer.php

---
+++ transfer.php
@@ -0,0 +1,18 @@
+<?php 
+//Adding Lib for SuperiorCoin Functions
+require "../vendor/autoload.php";
+use Superior\Wallet;
+$walletFaucet = new Superior\Wallet();
+//$balanceFaucet = $walletFaucet->getBalance();
+
+$pablo= '5NKJdxdiCmccLyw53D8MzUhZYzDDvdBXshrVhUgYSYjyJFk3Wn5bMjsDSCxzSi1d95M83fENY7uEmUm5t2Uj8rGEFXFTQ3q';
+
+$tx_hash = $walletFaucet->transfer($options);
+$options = [
+    'destinations' => (object) [
+        'amount' => '2',
+        'address' => $pablo
+    ]
+];
+
+?>
