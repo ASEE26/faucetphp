35f836a91a00b9ac9466412c287111b8b2fb9849
cesardanielhg <cesardanielhg@gmail.com>
2018-03-29 17:18:16 -0600

    trasnfer.php 3

--- transfer.php
+++ transfer.php
@@ -7,13 +7,14 @@ $walletFaucet = new Superior\Wallet();
 //$balanceFaucet = $walletFaucet->getBalance();
 
 $pablo= '5NKJdxdiCmccLyw53D8MzUhZYzDDvdBXshrVhUgYSYjyJFk3Wn5bMjsDSCxzSi1d95M83fENY7uEmUm5t2Uj8rGEFXFTQ3q';
-
-$tx_hash = $walletFaucet->transfer($options);
 $options = [
     'destinations' => (object) [
         'amount' => '4',
         'address' => $pablo
     ]
 ];
+
+
+$tx_hash = $walletFaucet->transfer($options);
 
 ?>
