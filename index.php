2f580f89a461e12e14866a46f0cee79a6e7d6dbc
cesardanielhg <cesardanielhg@gmail.com>
2018-03-29 14:19:15 -0600

    testing balance var 3

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ require_once "maincore.php";
 
 //Adding Lib for SuperiorCoin Functions
 require "../vendor/autoload.php";
-use Superior\Wallet;
-$walletFaucet = new Superior\Wallet();
 
 
 
@@ -204,6 +202,10 @@ $balanceFaucet = $walletFaucet->getBalance();
 */
 
 //$balance = $this->getBalance();
+use Superior\Wallet;
+$walletFaucet = new Superior\Wallet();
+$balanceFaucet = $walletFaucet->getBalance();
+
 $getfaucetbal = json_decode($balanceFaucet);
 $realBalance = number_format($getfaucetbal->{'balance'}/100000000);
 //$realBalance = number_format($realBalance, 2, '.', '');
