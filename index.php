f38174c02362df2da263e948992e7c3c58bbaf4c
cesardanielhg <cesardanielhg@gmail.com>
2018-03-29 14:12:20 -0600

    adding faucet functions

--- index.php
+++ index.php
@@ -201,18 +201,18 @@ $walletFaucet = new Superior\Wallet();
 
 $addressFaucet = $walletFaucet->getAddress();
 $balanceFaucet = $walletFaucet->getBalance();
-
+*/
 
 //$balance = $this->getBalance();
 $getfaucetbal = json_decode($balanceFaucet);
 $realBalance = number_format($getfaucetbal->{'balance'}/100000000);
-//$realBalance = strval(number_format($realBalance, 2, '.', ''));
+$realBalance = number_format($realBalance, 2, '.', '');
 $realBalance2 ="129,404";
 //return $realBalance;
 //print_r($realBalance);
 //$smarty->assign('faucet_balance',$realBalance);
 
-*/
+
 
 
 
