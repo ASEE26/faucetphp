51743e2444bf95e0f3b22544e7af95ee6ebc0973
cesardanielhg <cesardanielhg@gmail.com>
2018-03-29 14:17:41 -0600

    testing balance var 2

--- index.php
+++ index.php
@@ -206,7 +206,7 @@ $balanceFaucet = $walletFaucet->getBalance();
 //$balance = $this->getBalance();
 $getfaucetbal = json_decode($balanceFaucet);
 $realBalance = number_format($getfaucetbal->{'balance'}/100000000);
-$realBalance = number_format($realBalance, 2, '.', '');
+//$realBalance = number_format($realBalance, 2, '.', '');
 $realBalance2 ="129,404";
 //return $realBalance;
 //print_r($realBalance);
